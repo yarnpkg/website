ea58899d51bfe9dc86aeef614dac6472f1f403fb
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2016-12-09 03:58:35 +0300

    Fix install.sh (#309)
    * Fix https://github.com/yarnpkg/yarn/issues/2061 (https://github.com/yarnpkg/yarn/pull/2071)

--- install.sh
+++ install.sh
@@ -156,7 +156,7 @@ yarn_install() {
   printf "${white}Installing Yarn!$reset\n"
 
   if [ -d "$HOME/.yarn" ]; then
-    if [ -n `which yarn` ]; then
+    if which yarn; then
       local latest_url
       local specified_version
       local version_type
@@ -180,8 +180,8 @@ yarn_install() {
         rm -rf "$HOME/.yarn"
       fi
     else
-      printf "$red> ~/.yarn already exists, possibly from a past Yarn install.$reset\n"
-      printf "$red> Remove it (rm -rf ~/.yarn) and run this script again.$reset\n"
+      printf "$red> $HOME/.yarn already exists, possibly from a past Yarn install.$reset\n"
+      printf "$red> Remove it (rm -rf $HOME/.yarn) and run this script again.$reset\n"
       exit 0
     fi
   fi
