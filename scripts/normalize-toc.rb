4b273246021da17afcfb3fa376f824bc4767c11c
james kyle <me@thejameskyle.com>
2017-01-31 15:22:01 -0800

    Prepare site for translation (#364)
    * Setup better redirects
    
    * Fix frontmatter script
    
    * Fix encoding issue in scripts
    
    * re-disable languages

---
+++ scripts/normalize-toc.rb
@@ -0,0 +1,16 @@
+sourceRegex = /\[\]\(#toc-(.*?)\)\{#toc-.*?\}/
+
+langsDir = File.join('.', 'lang')
+langs = Dir.entries(langsDir).drop(2).reject {|lang| lang == 'en' }
+
+langs.each {|lang|
+  files = Dir.glob(File.join(langsDir, lang, '**', '*.*'))
+  file = files[20]
+  files.each {|file|
+    contents = IO.read file
+    result = contents
+      .encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+      .gsub(sourceRegex, '<a class="toc" id="toc-\1" href="#toc-\1"></a>')
+    File.write(file, result)
+  }
+}
