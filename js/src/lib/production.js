807f8fb61da0b3729d575342ca582fb50c04474d
Haroen Viaene <hello@haroen.me>
2017-02-10 15:28:26 +0100

    fix typo and add polyfill

--- js/src/lib/production.js
+++ js/src/lib/production.js
@@ -1,9 +1,13 @@
+import "babel-polyfill";
+
 export function fillLanguageDropdown() {
   // Takes over language dropdown to set cookie
   const langMenu = document.getElementById('dropdownNavLanguageMenu');
-  const langMenuItems = langMenu.getElementsByClassName('dropdown-item');
+  const langMenuItems = langMenu.querySelectorAll('dropdown-item');
 
-  for (const langMenuItem of langMenuItems) {
+  console.log(typeof langMenuItems);
+
+  langMenuItems.forEach((langMenuItem) => {
     langMenuItem.addEventListener('click', event => {
       event.preventDefault();
       const target = event.currentTarget;
@@ -11,5 +15,5 @@ export function fillLanguageDropdown() {
       document.cookie = `nf_lang=${lang}`;
       location.reload();
     });
-  }
+  });
 }
