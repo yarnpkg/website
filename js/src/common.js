e8873dae5b6fdb6c203d58393787793f646805a0
Haroen Viaene <hello@haroen.me>
2017-02-14 09:15:36 +0100

    move order of operations

--- js/src/common.js
+++ js/src/common.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { fillLanguageDropdown } from './lib/production';
 import Search from './lib/Search';
 
-if (process.env.NODE_ENV === 'production') {
-  fillLanguageDropdown();
-}
-
 ReactDOM.render(
   <Search />,
   document.getElementById('search')
 );
+
+if (process.env.NODE_ENV === 'production') {
+  fillLanguageDropdown();
+}
