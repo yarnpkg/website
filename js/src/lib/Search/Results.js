42a8c7eaa88070fa17a863df5bf5d0e08c5d5fb2
Haroen Viaene <hello@haroen.me>
2017-02-15 13:58:37 +0100

    make <main> and reset appear/disappear

--- js/src/lib/Search/Results.js
+++ js/src/lib/Search/Results.js
@@ -4,6 +4,8 @@ import { Hits, Pagination } from 'react-instantsearch/dom';
 import Hit from './Hit';
 import { isEmpty } from './util';
 
+const body = document.querySelector('body');
+
 const Results = createConnector({
   displayName: 'ConditionalResults',
   getProvidedProps(props, searchState, searchResults) {
@@ -12,10 +14,13 @@ const Results = createConnector({
   },
 })(({noResults, query}) => {
   if (isEmpty(query)) {
+    body.classList.remove('searching');
     return <span></span>;
   } else if (noResults) {
+    body.classList.add('searching');
     return <div>No results have been found for {query}</div>;
   } else {
+    body.classList.add('searching');
     return <div className="container">
       <Hits hitComponent={Hit}/>
       <div className="d-flex">
