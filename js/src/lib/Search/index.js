13e95117cf89a9fe916fcf337e6cec167122f314
vvo <vincent.voyer@algolia.com>
2017-02-16 17:55:24 -0800

    fix(url): good url sync handling

--- js/src/lib/Search/index.js
+++ js/src/lib/Search/index.js
@@ -3,71 +3,27 @@ import qs from 'qs'
 import { InstantSearch, Configure, SearchBox } from 'react-instantsearch/dom';
 import isEqual from 'lodash/isEqual';
 import Results from './Results';
+import withUrlSync from './withUrlSync';
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.lastPush = 0;
-    this.originalURL = location.href;
-    this.previousURL = '';
-    this.state = {
-      searchState: {
-        ...qs.parse(location.search.substring(1))
-      }
-    }
-  }
+const Search = props =>
+  <InstantSearch
+    appId='OFCNCOG2CU'
+    apiKey='f54e21fa3a2a0160595bb058179bfb1e'
+    indexName='npm-search'
+    searchState={props.searchState}
+    onSearchStateChange={props.onSearchStateChange}
+  >
+    <Configure
+      hitsPerPage={5}
+      // optionalFacetFilters={`name:${this.state.searchState.query}`}
+      facets={['keywords']}
+    />
+    <SearchBox
+      translations={{
+        placeholder: "Search packages (i.e. babel, webpack, react…)"
+      }}
+    />
+    <Results />
+  </InstantSearch>;
 
-  componentWillReceiveProps() {
-    this.setState({searchState: qs.parse(location.search.substring(1))});
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return !isEqual(this.state.searchState, nextState.searchState);
-  }
-
-  onSearchStateChange(nextSearchState) {
-    const THRESHOLD = 700;
-    const newPush = Date.now();
-    this.setState({searchState: nextSearchState});
-    console.log(nextSearchState.configure, nextSearchState);
-    delete nextSearchState.configure; // <Configure /> goes first
-    if (nextSearchState.query.length < 1) {
-      if (this.previousURL !== this.originalURL) {
-        this.previousURL = this.originalURL;
-        history.pushState({}, document.title, this.originalURL);
-      }
-    } else if (newPush - this.lastPush >= THRESHOLD) {
-      this.lastPush = newPush;
-      history.replaceState({}, 'Yarn Search', this.createURL(nextSearchState));
-    }
-  }
-
-  createURL = state => `/packages?${qs.stringify(state)}`;
-
-  render() {
-    return (
-      <InstantSearch
-        appId='OFCNCOG2CU'
-        apiKey='f54e21fa3a2a0160595bb058179bfb1e'
-        indexName='npm-search'
-        className='pkg-search'
-        searchState={this.state.searchState}
-        onSearchStateChange={this.onSearchStateChange.bind(this)}
-      >
-        <Configure
-          hitsPerPage={5}
-          optionalFacetFilters={`name:${this.state.searchState.query}`}
-          facets={['keywords']}
-        />
-        <SearchBox 
-          translations={{
-            placeholder: "Search packages (i.e. babel, webpack, react…)"
-          }}
-        />
-        <Results />
-      </InstantSearch>
-    );
-  }
-};
-
-export default Search;
+export default withUrlSync(Search);
---
+++ js/src/lib/Search/withUrlSync.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import qs from 'qs';
+
+const updateAfter = 700;
+const searchStateToQueryString = searchState => ({q: searchState.query});
+const searchStateToUrl = searchState => searchState ? `/packages?${qs.stringify(searchStateToQueryString(searchState))}` : '';
+const queryStringToSearchState = queryString => ({query: qs.parse(queryString).q});
+const originalPathName = location.pathname;
+
+export default App => class extends Component {
+  constructor() {
+    super();
+    this.state = {searchState: queryStringToSearchState(location.search.slice(1))};
+    console.log(this.state)
+    window.addEventListener( // check we are on a search result
+      'popstate',
+      ({state: searchState}) => {
+        if (searchState !== null) {
+          this.setState({searchState});
+          return;
+        }
+
+        this.setState({searchState: {query: ''}});
+      }
+    );
+  }
+
+  onSearchStateChange = searchState => {
+    clearTimeout(this.debouncedSetState);
+
+    if (searchState.query === '') {
+      if (location.pathname !== originalPathName) {
+        window.history.pushState(null, null, originalPathName);
+      }
+    } else {
+      this.debouncedSetState = setTimeout(() => {
+        window.history.pushState(
+          searchState,
+          null,
+          searchStateToUrl(searchState)
+        );
+      }, updateAfter);
+    }
+
+    this.setState({searchState});
+  };
+
+  render() {
+    return <App {...this.props}
+      searchState={this.state.searchState}
+      onSearchStateChange={this.onSearchStateChange.bind(this)}
+      createURL={searchStateToUrl}
+    />;
+  }
+};
