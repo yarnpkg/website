87adcafb9c248e0e0b02744018834e1d11574ac9
Haroen Viaene <hello@haroen.me>
2017-02-20 18:27:47 +0100

    feat(details): create components for links

--- js/src/lib/Details/index.js
+++ js/src/lib/Details/index.js
@@ -2,11 +2,32 @@ import React from 'react';
 import algoliasearch from 'algoliasearch';
 
 import { License, Owner } from '../Hit';
-import { Keywords } from '../util';
+import { Keywords, encode } from '../util';
 import schema from '../schema';
 
 const client = algoliasearch('OFCNCOG2CU', 'f54e21fa3a2a0160595bb058179bfb1e');
 const index = client.initIndex('npm-search');
+
+const Link = ({ site, url }) => (
+  <a href={url} className={`details-link--${site}`}>{url}</a>
+);
+
+const Links = ({ name, homepage, githubRepo, className }) => (
+  <div className="detail-links">
+    <Link site="npm" url={`https://www.npmjs.com/package/${name}`} />
+    {githubRepo
+      ? <Link
+          site="github"
+          url={
+            `https://github.com/${encode(githubRepo.user)}/${encode(
+              githubRepo.project,
+            )}${githubRepo.path}`
+          }
+        />
+      : null}
+    {homepage ? <Link name="homepage" url={homepage} /> : null}
+  </div>
+);
 
 class Details extends React.Component {
   constructor(props) {
@@ -33,6 +54,11 @@ class Details extends React.Component {
         <Owner {...this.state.owner} />
         <License type={this.state.license} />
         <Keywords keywords={this.state.keywords} />
+        <Links
+          name={this.state.name}
+          homepage={this.state.homepage}
+          githubRepo={this.state.githubRepo}
+        />
         <pre>
           {JSON.stringify(this.state, null, '  ')}
         </pre>
