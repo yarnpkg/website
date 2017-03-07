776cd1c5707a28198d064d0990669933757e854c
Haroen Viaene <hello@haroen.me>
2017-03-07 09:48:49 +0100

    feat(details) parse links in readme correctly

--- js/src/lib/Details/index.js
+++ js/src/lib/Details/index.js
@@ -10,27 +10,34 @@ import schema from '../schema';
 const client = algoliasearch('OFCNCOG2CU', 'f54e21fa3a2a0160595bb058179bfb1e');
 const index = client.initIndex('npm-search');
 
-//   markedRenderer.image = (href, title, text) => {
-// return `<img src="${href.indexOf('//') > 0
-//   ? href
-//   : `https://raw.githubusercontent.com/${this.state.githubRepo.user}/${this.state.githubRepo.project}/${this.state.githubRepo.path
-//       ? this.state.githubRepo.path.replace(/\/tree\//, '')
-//       : 'master'}/${href.replace(/\.\//, '')}`}" ${title
-//   ? `title="${title}"`
-//   : ''} ${text ? `alt="${text}"` : ''}  />`;
-
-const prefixGitHub = (uri, head) => uri;
+const prefixURL = (url, { base, user, project, head, path }) => {
+  if (url.indexOf('//') > 0) {
+    return url;
+  } else {
+    return new URL(
+      url.replace(/^\.?\//, ''),
+      `${base}/${user}/${project}/${head}/${path}`,
+    );
+  }
+};
 
 const Markdown = ({ source, githubRepo: { user, project, path }, gitHead }) => (
   <ReactMarkdown
     source={source}
-    transformLinkUri={uri => {
-      return prefixGitHub(uriTransformer(uri), gitHead);
-    }}
-    transformImageUri={uri => {
-      console.log('image', uri);
-      return prefixGitHub(uriTransformer(uri), gitHead);
-    }}
+    transformLinkUri={url => prefixURL(uriTransformer(url), {
+      base: 'https://github.com',
+      user,
+      project,
+      head: gitHead ? `tree/${gitHead}` : 'tree/master',
+      path,
+    })}
+    transformImageUri={url => prefixURL(uriTransformer(url), {
+      base: 'https://raw.githubusercontent.com',
+      user,
+      project,
+      head: gitHead ? gitHead : 'master',
+      path,
+    })}
   />
 );
 
@@ -95,15 +102,16 @@ class Details extends React.Component {
             license={this.state.license}
             keywords={this.state.keywords}
           />
-          {this.state.readme &&
-            <section id="readme">
-              <h3>Readme</h3>
-              <Markdown
-                source={this.state.readme}
-                githubRepo={this.state.githubRepo}
-                gitHead={this.state.gitHead}
-              />
-            </section>}
+          <section id="readme">
+            <h3>Readme</h3>
+            {this.state.readme
+              ? <Markdown
+                  source={this.state.readme}
+                  githubRepo={this.state.githubRepo}
+                  gitHead={this.state.gitHead}
+                />
+              : <div>no readme found 😢</div>}
+          </section>
           {this.state.changelog &&
             <section id="changelog">
               <h3>Changelog</h3>
