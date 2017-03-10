10a6e13915a804f32d4faacc3009916694772140
Haroen Viaene <hello@haroen.me>
2017-03-10 12:15:24 +0100

    feat(detail) handle scoped packages in prod

--- js/src/package.js
+++ js/src/package.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Details from './lib/Details';
 
 const id = process.env.NODE_ENV == 'production'
-  ? location.pathname.split('/')[3]
+  ? location.pathname.split('/').slice(3).join('/')
   : location.search.substring(1);
 
 ReactDOM.render(
