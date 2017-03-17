e314b93adf99c0248bb8ea569cb7ee603196557a
Haroen Viaene <hello@haroen.me>
2017-03-17 18:41:27 +0100

    feat(details) add usage section

---
+++ js/src/lib/Details/Usage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Usage = ({ dependencies }) => (
+  <article className="details-side--usage">
+    <h1>Usage</h1>
+    <dl>
+      {dependencies &&
+        <div className="d-flex flex-items-between w-100">
+          <dt>
+            {Object.keys(dependencies).length > 0
+              ? <details>
+                  <summary>Dependencies</summary>
+                  {Object.keys(dependencies).join(', ')}
+                </details>
+              : 'Dependencies'}
+          </dt>
+          <span className="dotted flex-grow" />
+          <dd>{Object.keys(dependencies).length}</dd>
+        </div>}
+    </dl>
+  </article>
+);
+
+export default Usage;
