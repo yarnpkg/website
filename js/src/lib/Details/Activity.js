ff556ce0f781f6360a26c58c4fe3bdf01361460f
Haroen Viaene <hello@haroen.me>
2017-03-17 18:58:42 +0100

    feat(details) don't show activity if you're rate-limited

--- js/src/lib/Details/Activity.js
+++ js/src/lib/Details/Activity.js
@@ -15,36 +15,35 @@ const formatWeeksSinceLastCommit = weeks =>
     ? 'over a year ago'
     : `${weeks < 1 ? 'less than a' : weeks} week${weeks > 1 ? 's' : ''} ago`;
 
-const Activity = ({ data = [] }) => (
-  <article className="details-side--activity">
-    <h1>Activity</h1>
-    <Sparklines
-      data={data.map(week => week.total)}
-      width={100}
-      height={15}
-      limit={12 /*three months*/}
-    >
-      <SparklinesLine color="#2C8EBB" />
-    </Sparklines>
-    <dl>
-      {!isEmpty(data) &&
-        <div className="d-flex flex-items-between w-100">
-          <dt>Commits last 3 months</dt>
-          <span className="dotted flex-grow" />
-          <dd>
-            {commitsLastThreemonths({ weeklyData: data })}
-          </dd>
-        </div>}
-      {!isEmpty(data) &&
-        <div className="d-flex flex-items-between w-100">
-          <dt>Last commit</dt>
-          <span className="dotted flex-grow" />
-          <dd>
-            {weeksAgoSinceLastCommit({ weeklyData: data })}
-          </dd>
-        </div>}
-    </dl>
-  </article>
-);
+const Activity = ({ data = [] }) => !isEmpty(data) &&
+<article className="details-side--activity">
+  <h1>Activity</h1>
+  <Sparklines
+    data={data.map(week => week.total)}
+    width={100}
+    height={15}
+    limit={12 /*three months*/}
+  >
+    <SparklinesLine color="#2C8EBB" />
+  </Sparklines>
+  <dl>
+    <div className="d-flex flex-items-between w-100">
+      <img src="/assets/detail/ico-commits.svg" alt="" />
+      <dt>Commits last 3 months</dt>
+      <span className="dotted flex-grow" />
+      <dd>
+        {commitsLastThreemonths({ weeklyData: data })}
+      </dd>
+    </div>
+    <div className="d-flex flex-items-between w-100">
+      <img src="/assets/detail/ico-commits-last.svg" alt="" />
+      <dt>Last commit</dt>
+      <span className="dotted flex-grow" />
+      <dd>
+        {weeksAgoSinceLastCommit({ weeklyData: data })}
+      </dd>
+    </div>
+  </dl>
+</article>;
 
 export default Activity;
