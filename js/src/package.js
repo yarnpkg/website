7f9eec8e4325d9025d956217241add01d1578d41
Vincent Voyer <wouaren@gmail.com>
2017-03-24 14:48:12 +0100

    style(code): add comment on package id retrieval

--- js/src/package.js
+++ js/src/package.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Details from './lib/Details';
 
+/*
+  To get the package id:
+    - production: https://yarnpkg.com/en/package/@kadira/storybook => @kadira/storybook
+    - dev (no rewrite available): http://localhost:4000/lang/en/package/?@kadira/storybook => @kadira/storybook
+*/
 const id = process.env.NODE_ENV == 'production'
   ? location.pathname.split('/').slice(3).join('/')
   : location.search.substring(1);
