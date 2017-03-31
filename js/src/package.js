05fc8025e4bcf5ebd2dccbedc32daf8de190fb42
Haroen Viaene <fingebimus@me.com>
2017-03-31 18:47:25 +0200

    fix(detail): make changing languages work (#440)
    Since the url isn't aware of which package detail we're on, we need to set the url to the correct package.
    
    Works as good as it can in dev, but dev doesn't have full i18n
    
    * change forEach to for

--- js/src/package.js
+++ js/src/package.js
@@ -11,7 +11,20 @@ const id = process.env.NODE_ENV == 'production'
   ? location.pathname.split('/').slice(3).join('/')
   : location.search.substring(1);
 
+function languageDropdownAddPackage(pkg) {
+  const langMenu = document.getElementById('dropdownNavLanguageMenu');
+  const langMenuItems = langMenu.querySelectorAll('.dropdown-item');
+
+  const addPackage = langMenuItem => langMenuItem.href += `/${pkg}`;
+
+  for (let i = 0; i < langMenuItems.length; i++) {
+    addPackage(langMenuItems[i]);
+  }
+}
+
+languageDropdownAddPackage(id);
+
 ReactDOM.render(
   <Details objectID={id} />,
-  document.getElementById('pkg-detail'),
+  document.getElementById('pkg-detail')
 );
