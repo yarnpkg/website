174b87523101d86b32b816ec944783e09994805b
Haroen Viaene <hello@haroen.me>
2017-04-27 10:56:13 +0200

    feat(SEO): add a script to make sitemaps
    This will only be enabled on production deploys, but for now on this first preview too, to check if it works.
    
    It basically browses through all Algolia records and then makes a record for each language in a sitemap.
    
    This will after the merge be needed to be linked to Google Webmaster Tools.

---
+++ scripts/sitemaps.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+// commented out in first commit to test it on Netlify
+// will make sure that it this is only used on production deploys
+// if (process.env.CONTEXT === 'deploy-preview') {
+//   console.log('sitemap generation skipped');
+// } else {
+const algoliaSitemap = require('algolia-sitemap');
+
+const algoliaConfig = {
+  appId: 'OFCNCOG2CU',
+  apiKey: process.env.ALGOLIA_BROWSE_KEY,
+  indexName: 'npm-search',
+};
+
+function hitToParams(hit) {
+  const url = ({ lang, name }) => `https://yarnpkg.com/${lang}/package/${name}`;
+  const loc = url({ lang: 'en', name: hit.name });
+  const lastmod = new Date(hit.modified).toISOString();
+  const priority = hit.downloadsRatio;
+  return {
+    loc,
+    lastmod,
+    priority,
+    alternates: {
+      languages: ['fr', 'pt-BR', 'zh-Hans'],
+      hitToURL: lang => url({ lang, name: hit.name }),
+    },
+  };
+}
+
+algoliaSitemap({
+  algoliaConfig,
+  sitemapLocation: {
+    href: 'https://yarnpkg.com/sitemaps',
+    path: `${__dirname}/sitemaps`,
+  },
+  hitToParams,
+});
+// }
