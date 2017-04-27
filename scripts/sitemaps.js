238a8121301953495f115e0b349631cdf742fbc1
Haroen Viaene <hello@haroen.me>
2017-04-27 13:25:29 +0200

    don't run sitemap on deploy previews

--- scripts/sitemaps.js
+++ scripts/sitemaps.js
@@ -1,45 +1,44 @@
 #!/usr/bin/env node
 
-// commented out in first commit to test it on Netlify
-// will make sure that it this is only used on production deploys
-// if (process.env.CONTEXT === 'deploy-preview') {
-//   console.log('sitemap generation skipped');
-// } else {
-const algoliaSitemap = require('algolia-sitemap');
-const { mkdirSync } = require('fs');
+if (process.env.CONTEXT === 'deploy-preview') {
+  console.log('sitemap generation skipped');
+} else {
+  const algoliaSitemap = require('algolia-sitemap');
+  const { mkdirSync } = require('fs');
 
-const algoliaConfig = {
-  appId: 'OFCNCOG2CU',
-  apiKey: process.env.ALGOLIA_BROWSE_KEY,
-  indexName: 'npm-search',
-};
-
-function hitToParams(hit) {
-  const url = ({ lang, name }) => `https://yarnpkg.com/${lang}/package/${name}`;
-  const loc = url({ lang: 'en', name: hit.name });
-  const lastmod = new Date(hit.modified).toISOString();
-  const priority = hit.downloadsRatio;
-  return {
-    loc,
-    lastmod,
-    priority,
-    alternates: {
-      languages: ['fr', 'pt-BR', 'zh-Hans'],
-      hitToURL: lang => url({ lang, name: hit.name }),
-    },
+  const algoliaConfig = {
+    appId: 'OFCNCOG2CU',
+    apiKey: process.env.ALGOLIA_BROWSE_KEY,
+    indexName: 'npm-search',
   };
+
+  function hitToParams(hit) {
+    const url = ({ lang, name }) =>
+      `https://yarnpkg.com/${lang}/package/${name}`;
+    const loc = url({ lang: 'en', name: hit.name });
+    const lastmod = new Date(hit.modified).toISOString();
+    const priority = hit.downloadsRatio;
+    return {
+      loc,
+      lastmod,
+      priority,
+      alternates: {
+        languages: ['fr', 'pt-BR', 'zh-Hans'],
+        hitToURL: lang => url({ lang, name: hit.name }),
+      },
+    };
+  }
+
+  const path = `${__dirname}/../sitemaps`;
+
+  mkdirSync(`${__dirname}/../sitemaps`);
+
+  algoliaSitemap({
+    algoliaConfig,
+    sitemapLocation: {
+      href: 'https://yarnpkg.com/sitemaps',
+      path,
+    },
+    hitToParams,
+  });
 }
-
-const path = `${__dirname}/../sitemaps`;
-
-mkdirSync(`${__dirname}/../sitemaps`);
-
-algoliaSitemap({
-  algoliaConfig,
-  sitemapLocation: {
-    href: 'https://yarnpkg.com/sitemaps',
-    path,
-  },
-  hitToParams,
-});
-// }
