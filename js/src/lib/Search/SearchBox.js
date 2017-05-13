4be190890ab0fbe09adb41ceb4cf38f83577960a
Haroen Viaene <hello@haroen.me>
2017-05-13 16:33:59 +0200

    feat(Deprecation): show if a package is deprecated
    It's a badge just like the License one, but inversed
    
    (also included in this PR is moving from React.PropTypes -> PropTypes, apparently I didn't get that message when upgrading the deps yesterday)

--- js/src/lib/Search/SearchBox.js
+++ js/src/lib/Search/SearchBox.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { SearchBox } from 'react-instantsearch/dom';
 
 class WrappedSearchBox extends Component {
