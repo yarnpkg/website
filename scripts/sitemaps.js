3c1e16d6223818a06588f4d30fa21ea776397c05
Haroen Viaene <hello@haroen.me>
2017-06-22 16:20:02 +0200

    test if sitemap still works

--- scripts/sitemaps.js
+++ scripts/sitemaps.js
@@ -1,43 +1,38 @@
 #!/usr/bin/env node
 
-if (process.env.CONTEXT !== 'deploy-preview') {
-  console.log('sitemap generation skipped');
-} else {
-  const algoliaSitemap = require('algolia-sitemap');
-  const { mkdirSync, rmdirSync } = require('fs');
+const algoliaSitemap = require('algolia-sitemap');
+const { mkdirSync, rmdirSync } = require('fs');
 
-  const algoliaConfig = {
-    appId: 'OFCNCOG2CU',
-    apiKey: process.env.ALGOLIA_BROWSE_KEY,
-    indexName: 'npm-search',
+const algoliaConfig = {
+  appId: 'OFCNCOG2CU',
+  apiKey: process.env.ALGOLIA_BROWSE_KEY,
+  indexName: 'npm-search',
+};
+
+function hitToParams(hit) {
+  const url = ({ lang, name }) => `https://yarnpkg.com/${lang}/package/${name}`;
+  const loc = url({ lang: 'en', name: hit.name });
+  const lastmod = new Date(hit.modified).toISOString();
+  const priority = hit.downloadsRatio;
+  return {
+    loc,
+    lastmod,
+    priority,
+    alternates: {
+      languages: ['fr', 'pt-BR', 'zh-Hans'],
+      hitToURL: lang => url({ lang, name: hit.name }),
+    },
   };
-
-  function hitToParams(hit) {
-    const url = ({ lang, name }) =>
-      `https://yarnpkg.com/${lang}/package/${name}`;
-    const loc = url({ lang: 'en', name: hit.name });
-    const lastmod = new Date(hit.modified).toISOString();
-    const priority = hit.downloadsRatio;
-    return {
-      loc,
-      lastmod,
-      priority,
-      alternates: {
-        languages: ['fr', 'pt-BR', 'zh-Hans'],
-        hitToURL: lang => url({ lang, name: hit.name }),
-      },
-    };
-  }
-
-  const path = `${__dirname}/../sitemaps`;
-
-  rmdirSync(path);
-  mkdirSync(path);
-
-  algoliaSitemap({
-    algoliaConfig,
-    sitemapLoc: 'https://yarnpkg.com/sitemaps',
-    outputFolder: path,
-    hitToParams,
-  });
 }
+
+const path = `${__dirname}/../sitemaps`;
+
+rmdirSync(path);
+mkdirSync(path);
+
+algoliaSitemap({
+  algoliaConfig,
+  sitemapLoc: 'https://yarnpkg.com/sitemaps',
+  outputFolder: path,
+  hitToParams,
+});
