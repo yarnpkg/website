fe7611b9e1d5e67401772521174d9ede3e2a80ab
Burak Yiğit Kaya <ben@byk.im>
2017-09-06 00:02:31 +0300

    Fix: Make install.sh Alpine linux compatible (#623)
    * Fix: Make install.sh Alpine linux compatible
    
    **Summary**
    
    Alpine Linux comes with BusyBox tar, which lacks the `--strip` option. This patch updates the `install.sh` script to not rely on this option.
    
    **Test plan**
    
    Manually verified on local Ubuntu for Windows.
    
    * Respect humans

--- install.sh
+++ install.sh
@@ -33,8 +33,12 @@ yarn_get_tarball() {
     yarn_verify_integrity $tarball_tmp
 
     printf "$cyan> Extracting to ~/.yarn...$reset\n"
+    # All this dance is because `tar --strip=1` does not work everywhere
+    temp=$(mktemp -d)
+    tar zxf $tarball_tmp -C "$temp"
     mkdir .yarn
-    tar zxf $tarball_tmp -C .yarn --strip 1 # extract tarball
+    mv "$temp"/*/* .yarn
+    rm -rf "$temp"
     rm $tarball_tmp*
   else
     printf "$red> Failed to download $url.$reset\n"
