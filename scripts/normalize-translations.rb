13db81a34107e96f677ed5b4f4ea401be6bc4303
Haroen Viaene <fingebimus@me.com>
2017-09-07 19:03:31 +0200

    Update normalize-translations.rb

--- scripts/normalize-translations.rb
+++ scripts/normalize-translations.rb
@@ -11,17 +11,10 @@ tocRegex = /\[\]\(#toc-(.*?)\)\{#toc-.*?\}/
 langsDir = File.join('.', 'lang')
 langs = Dir.entries(langsDir).drop(2).reject {|lang| lang == 'en' }
 
-# Build up a hashmap with all of the source front-matters
-# { relativePath => frontMatter }
-englishFiles = Dir.glob(File.join(langsDir, 'en', '**', '*.*'))
-english = {}
-englishFiles.each {|file|
-  path = file.sub(File.join(langsDir, 'en'), '')
-  contents = IO.read file
-  contents = contents.unicode_normalize
-  result = contents.match frontmatterSourceRegex
-  english[path] = result
-}
-
 # haha easier to read
-puts langs
+puts "langsDir"
+puts langsDir
+puts "entries"
+puts Dir.entries(langsDir)
+puts "reject"
+puts Dir.entries(langsDir).drop(2).reject {|lang| lang == 'en' }
