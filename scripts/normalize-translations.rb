3cb22a8c231dd5670a6223a22c3988f163768214
Haroen Viaene <fingebimus@me.com>
2017-09-07 18:55:14 +0200

    Update normalize-translations.rb

--- scripts/normalize-translations.rb
+++ scripts/normalize-translations.rb
@@ -23,27 +23,5 @@ englishFiles.each {|file|
   english[path] = result
 }
 
-langs.each {|lang|
-  files = Dir.glob(File.join(langsDir, lang, '**', '*.*'))
-  # debugging: what is `files`?
-  puts files
-
-  files.each {|file|
-    puts "- Replacing #{file}"
-
-    path = file.sub(File.join(langsDir, lang), '')
-    contents = IO.read file
-    frontmatter = english[path]
-    # this is for debugging, I want to know which line fails
-    puts contents
-
-    result = contents
-    result = result.unicode_normalize
-    result = result.gsub /\r\n?/, "\n"
-    result = result + "\n"
-    result = result.gsub(tocRegex, '<a class="toc" id="toc-\1" href="#toc-\1"></a>')
-    result = result.gsub(frontmatterTargetRegex) {|c| frontmatter } if frontmatter
-
-    File.write(file, result)
-  }
-}
+# haha easier to read
+puts langs
