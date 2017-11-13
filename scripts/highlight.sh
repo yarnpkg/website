e1e4718cd5f30d820ae83c44e54037a6bf0f08c2
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-11-13 12:52:40 -0200

    Removed submodule

--- scripts/highlight.sh
+++ scripts/highlight.sh
@@ -4,12 +4,6 @@ ROOT_PATH=$(python -c "import os,sys;print(os.path.dirname(os.path.realpath(sys.
 ROOT_PATH=$(dirname $ROOT_PATH)
 OLD_CWD=$(pwd)
 echo "Changing to $ROOT_PATH/_js/highlight"
-if [ -d "$ROOT_PATH/_js/highlight" ]; then
-  echo "highlight folder not found. Will init."
-  cd $ROOT_PATH
-  git submodule update --init --recursive
-fi
-
 cd $ROOT_PATH/_js/highlight
 rm -rf build/ &> /dev/null
 yarn
