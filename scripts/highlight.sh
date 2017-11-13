abc97051f572fa54507b6428f2bef554666a61a4
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-11-13 12:20:43 -0200

    Fixed build script

--- scripts/highlight.sh
+++ scripts/highlight.sh
@@ -2,9 +2,9 @@
 
 ROOT_PATH=$(python -c "import os,sys;print(os.path.dirname(os.path.realpath(sys.argv[1])))" $0)
 ROOT_PATH=$(dirname $ROOT_PATH)
-npm --prefix $ROOT_PATH/_js/highlight install
 OLD_CWD=$(pwd)
 cd $ROOT_PATH/_js/highlight
 rm -rf build/ &> /dev/null
+npm install
 node tools/build.js css javascript markdown bash shell xml typescript scss
 cd $OLD_CWD
