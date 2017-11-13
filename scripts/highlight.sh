5225bba17d4757659ae36c0346dbf2122a3fba96
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-11-13 12:23:17 -0200

    Fixed build script to use yarn

--- scripts/highlight.sh
+++ scripts/highlight.sh
@@ -5,6 +5,6 @@ ROOT_PATH=$(dirname $ROOT_PATH)
 OLD_CWD=$(pwd)
 cd $ROOT_PATH/_js/highlight
 rm -rf build/ &> /dev/null
-npm install
+yarn
 node tools/build.js css javascript markdown bash shell xml typescript scss
 cd $OLD_CWD
