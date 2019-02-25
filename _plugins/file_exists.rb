fe50d77f15a90dbb27be13b7edb6ee59ef1a402b
Haroen Viaene <hello@haroen.me>
2019-02-25 11:23:15 +0100

    fix(ci): fallback in case of non-existing page

---
+++ _plugins/file_exists.rb
@@ -0,0 +1,24 @@
+# source: https://github.com/michaelx/jekyll_file_exists/blob/dd363223754836a640de81ddab68fd95cebe7791/file_exists.rb
+module Jekyll
+  class FileExistsTag < Liquid::Tag
+
+      def initialize(tag_name, path, tokens)
+          super
+          @path = path
+      end
+
+      def render(context)
+          # Pipe parameter through Liquid to make additional replacements possible
+          url = Liquid::Template.parse(@path).render context
+
+          # Adds the site source, so that it also works with a custom one
+          site_source = context.registers[:site].config['source']
+          file_path = site_source + '/' + url
+
+          # Check if file exists (returns true or false)
+          "#{File.exist?(file_path.strip!)}"
+      end
+  end
+end
+
+Liquid::Template.register_tag('file_exists', Jekyll::FileExistsTag)
